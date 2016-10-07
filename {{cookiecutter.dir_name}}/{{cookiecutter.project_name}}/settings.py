39ccc4a33ff53560b95b1979f9b9d2e72e3fa730
Eric Rasche <rasche.eric@gmail.com>
2016-10-07 23:11:11 +0000

    Allow RO

--- {{cookiecutter.dir_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.dir_name}}/{{cookiecutter.project_name}}/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
